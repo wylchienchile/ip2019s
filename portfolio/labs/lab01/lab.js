b611e73f89d0c533763f81e1573d53dbc7f80ded
wylchienchile <52515333+wylchienchile@users.noreply.github.com>
2019-07-05 18:00:17 +0800

    Add files via upload

--- portfolio/labs/lab01/lab.js
+++ portfolio/labs/lab01/lab.js
@@ -3,9 +3,40 @@
 */
 
 
-function start(e) {
-
+function start (e){
+    var name = [
+    {
+        char: '簡',
+        cns: '1-767C',
+        big5: 'C2B2',
+        unicode: '7C21'
+    },
+    {
+        char: '綺',
+        cns: '1-6A3F',
+        big5: 'BAF6',
+        unicode: '7DBA'
+    },
+    {
+        char: '樂',
+        cns: '1-6D40',
+        big5: 'BCD6',
+        unicode: '6A02'
+    }
+    ]
+    var ele =document.getElementById("div1");
+    for (var i =0; i<3; i++) {
+        ele.innerHTML += name[i].char;
+        ele.innerHTML += ' cns=';
+        ele.innerHTML += name[i].cns;
+        ele.innerHTML += ' big5=';
+        ele.innerHTML += name[i].big5;
+        ele.innerHTML += ' unicode=';
+        ele.innerHTML += name[i].unicode;
+        ele.innerHTML += '</br>';
+    }
 }
+
 
 /*
 https://www.w3schools.com/jsref/met_document_addeventlistener.asp
--- portfolio/labs/lab02/lab.js
+++ portfolio/labs/lab02/lab.js
@@ -1,9 +1,25 @@
 /*
 
 */
-
+function update() {
+    var x =document.getElementById("in1").value;
+    var ele=document.getElementById('info2');
+    var y ='';
+    for(var i=0;i<x.length;i++){
+        y += x.charAt(i);
+        y +=' U+';
+        y += x.charCodeAt(i).toString(16).toUpperCase();
+        y += '<br>';
+    }
+    ele.innerHTML = y;
+    document.getElementById('info1').innerHTML=
+    document.getElementById('in1').value.replace('厭世', '人生好難')
+}
 
 function start(e) {
+    document.getElementById('in1').addEventListener("input",update);
+    update();
+
 
 }
 
